fb88e8802847805744352cdf047bd1a2df52f31d
sohyeon <ksj8595sh@pusan.ac.kr>
2026-07-30 00:17:42 +0900

    chore: initialize frontend and backend environments

---
+++ memory-drawer/backend/src/main/java/com/memorydrawer/MemoryDrawerBackendApplication.java
@@ -0,0 +1,12 @@
+package com.memorydrawer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryDrawerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryDrawerBackendApplication.class, args);
+	}
+}
